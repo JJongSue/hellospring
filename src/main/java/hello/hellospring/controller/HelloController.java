226fb79dbfeb9f14df2327dbf1aa3f4784c8922b
jspark940321 <jspark940321@gmail.com>
2021-04-11 01:30:04 +0900

    2021-04-10스프링 강의 코드

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,68 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @GetMapping("hello-int")
+    @ResponseBody
+    public Hello helloInt(@RequestParam("a") int a){
+        Hello hello = new Hello();
+        hello.setA(a);
+        hello.setName("nothing");
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+        private int a;
+
+        public int getA() {
+            return a;
+        }
+
+        public void setA(int a) {
+            this.a = a;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+}
