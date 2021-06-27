d8286bc47b724429e2089b3b70fe57771fa4b539
jspark940321 <jspark940321@gmail.com>
2021-06-27 15:45:13 +0900

    회원 리포지토리 테스트 케이스 작성

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -50,13 +51,32 @@ class MemoryMemberRepositoryTest {
 
     @Test
     void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
 
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
     }
 
     @Test
     void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
 
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
 
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
     }
 
 
